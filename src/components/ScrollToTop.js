38baaf691cd77294f2d7b851b508aa5ae68062ea
Fatima Karimli <58202681+Kerimli98@users.noreply.github.com>
2022-01-06 23:37:50 +0400

    first commit

---
+++ src/components/ScrollToTop.js
@@ -0,0 +1,11 @@
+import {useState, useEffect} from "react";
+
+function BtnToTop(props) {
+    return (
+        <div className="">
+            <i className="fas fa-chevron-up"></i>
+        </div>
+    );
+}
+
+export default BtnToTop;
